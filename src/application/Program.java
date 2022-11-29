6b609d1fa570ec2203b2fd05dfbbadab72ea5052
Rogpe-Prog <paulo.rogpe@gmail.com>
2022-11-29 07:32:40 -0300

    direct lambda

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,6 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -17,9 +16,7 @@ List<Product> list = new ArrayList<>();
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		Function<Product, String> func = p -> p.getName().toUpperCase();
-		
-		List<String> name = list.stream().map(func).collect(Collectors.toList());
+		List<String> name = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
 		
 		name.forEach(System.out::println);
 	}

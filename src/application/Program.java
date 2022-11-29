73013546534a31ba464a2f7b91b63b2c328ce50a
Rogpe-Prog <paulo.rogpe@gmail.com>
2022-11-29 07:31:53 -0300

    declared lambda

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -16,7 +17,9 @@ List<Product> list = new ArrayList<>();
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		List<String> name = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+		Function<Product, String> func = p -> p.getName().toUpperCase();
+		
+		List<String> name = list.stream().map(func).collect(Collectors.toList());
 		
 		name.forEach(System.out::println);
 	}
